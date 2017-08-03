0bc49b0c303e7c4748b0940934cbb3fff90fe695
Irony-J <jztirony@gmail.com>
2017-08-03 11:22:18 +1000

    first

---
+++ FastIO.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+void fastScan(int &num)
+{
+	bool negative = false;
+	register int c;
+
+	num = 0;
+
+	c = getchar_unlocked();//getchar()
+
+	if(c == '-'){
+		negative = true;
+		c = getchar();
+	}
+
+	for(; (c>='0' && c<='9');c=getchar_unlocked())
+		num = num * 10 + c - '0';
+}
+
+int main(){
+
+	int num;
+	fastScan(num);
+	printf("number is %d\n",num);
+}
